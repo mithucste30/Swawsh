d184bedddeefdec7ad8faad83d19ae575f398e50
Md. Mazharul islam Mithu <mithucste30@users.noreply.github.com>
2019-04-17 09:56:10 +0600

    change dep version to 1.0.9

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Swawsh",
     dependencies: [
-	.Package(url: "https://github.com/IBM-Swift/BlueCryptor.git")
+	.Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", from: "1.0.9")
     ]
 )
