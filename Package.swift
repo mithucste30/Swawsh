ccea1716566da86c988cbed34e0db3901cbfa52c
Md. Mazharul islam Mithu <mithucste30@users.noreply.github.com>
2019-04-17 10:13:30 +0600

    update package format

--- Package.swift
+++ Package.swift
@@ -4,5 +4,8 @@ let package = Package(
     name: "Swawsh",
     dependencies: [
 	.Package(url: "https://github.com/IBM-Swift/BlueCryptor.git", from: "1.0.9")
+    ],
+targets: [
+        .target(name: "Swawsh", dependencies: ["Cryptor"])
     ]
 )
